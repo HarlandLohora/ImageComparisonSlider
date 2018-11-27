5dd074bfc18c6f8e9e0a5b64e09237ea71380700
HarlandLohora <lohoraharland@gmail.com>
2018-11-26 18:44:31 -0600

    TrabajandoenJS

---
+++ script.js
@@ -0,0 +1,25 @@
+function initComparisons(){
+  var x, i;
+  x = document.getElementsByClassName('img2');
+  for (let i = 0; i < x.length; i++){
+    compareImages(x[i]);
+  }
+}
+function compareImages(img){
+  var slider, img, clicked = 0, w, h;
+  w = img.offsetWidth;
+  h = img.offserHeight;
+
+  img.style.width = ( w/2 ) + 'px';
+
+  slider = document.createElement( 'div' );
+  slider.setAttribute('class', 'imageBox-slider');
+
+  img.parentElement.insertBefore( slider, img );
+
+  slider.style.top  = ( h/2 ) - ( slider.offsetHeight/2 ) + 'px';
+  slider.style.left = ( w/2 ) - ( slider.offsetWidth/2  ) + 'px';
+  
+}
+
+initComparisons();
